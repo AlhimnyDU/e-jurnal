62de46486903cf65ee37f1a00799523ad80f113c
anisaputrisetyaningrum@mhs.itenas.ac.id <anisaputrisetyaningrum@mhs.itenas.ac.id>
2020-11-09 19:27:17 +0700

    revisian8

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://seminar.ratmi.itenas.ac.id/';
-$config['base_url'] = 'http://localhost/e-jurnal/';
+$config['base_url'] = 'http://seminar.ratmi.itenas.ac.id/';
+// $config['base_url'] = 'http://localhost/e-jurnal/';
 
 /*
 |--------------------------------------------------------------------------
