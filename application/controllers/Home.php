dfe8bfa048c752e8a9fe029f162e061fd25fc6df
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-01-28 09:08:11 +0700

    arsip jurnal

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -18,48 +19,55 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-    public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
-		$this->load->helper(array('form','url','download'));
-        $this->load->library('session');
-        $this->load->database();
+		$this->load->helper(array('form', 'url', 'download'));
+		$this->load->library('session');
+		$this->load->database();
 	}
 
-	public function index(){
-        $data['jurnal'] = $this->db->select('tbl_jurnal.*, tbl_akun.*')->from('tbl_jurnal')->join('tbl_akun','tbl_akun.id_akun=tbl_jurnal.id_akun','left')->where('tbl_jurnal.publish','y')->get()->result();
-        $this->load->view('home/templates/header');
-        $this->load->view('home/home',$data);
-        $this->load->view('home/templates/footer');
-	}
-	
-	public function tentangkami(){
-        $this->load->view('home/templates/header');
-        $this->load->view('home/tentang_kami');
-        $this->load->view('home/templates/footer');
-	}
-	
-	public function terkini(){
-        $this->load->view('home/templates/header');
-        $this->load->view('home/terkini');
-        $this->load->view('home/templates/footer');
-	}
-	
-	public function informasi(){
-        $this->load->view('home/templates/header');
-        $this->load->view('home/informasi');
-        $this->load->view('home/templates/footer');
-	}
-	
-	public function arsip(){
-		$data['jurnal'] = $this->db->select('*')->from('arsip')->get()->result();
-		// $data['jurnal'] = $this->db->select('tbl_jurnal.*, tbl_akun.nama, tbl_file.*')->from('tbl_jurnal')->join('tbl_akun','tbl_akun.id_akun=tbl_jurnal.id_akun','left')->join('tbl_file','tbl_file.id_jurnal=tbl_jurnal.id_jurnal','left')->where('tbl_jurnal.publish','y')->group_by('tbl_file.id_jurnal')->get()->result();
-        $this->load->view('home/templates/header');
-        $this->load->view('home/arsip',$data);
-        $this->load->view('home/templates/footer');
+	public function index()
+	{
+		$data['jurnal'] = $this->db->select('tbl_jurnal.*, tbl_akun.*')->from('tbl_jurnal')->join('tbl_akun', 'tbl_akun.id_akun=tbl_jurnal.id_akun', 'left')->where('tbl_jurnal.publish', 'y')->get()->result();
+		$this->load->view('home/templates/header');
+		$this->load->view('home/home', $data);
+		$this->load->view('home/templates/footer');
 	}
 
-	public function download_jurnal($nama_file){    
-        force_download('./assets/upload/jurnal/arsip/'.$nama_file, NULL);
+	public function tentangkami()
+	{
+		$this->load->view('home/templates/header');
+		$this->load->view('home/tentang_kami');
+		$this->load->view('home/templates/footer');
+	}
+
+	public function terkini()
+	{
+		$this->load->view('home/templates/header');
+		$this->load->view('home/terkini');
+		$this->load->view('home/templates/footer');
+	}
+
+	public function informasi()
+	{
+		$this->load->view('home/templates/header');
+		$this->load->view('home/informasi');
+		$this->load->view('home/templates/footer');
+	}
+
+	public function arsip()
+	{
+		// $data['jurnal'] = $this->db->select('*')->from('arsip')->get()->result();
+		$data['jurnal'] = $this->db->select('tbl_jurnal.*, tbl_akun.nama, tbl_file.*')->from('tbl_jurnal')->join('tbl_akun', 'tbl_akun.id_akun=tbl_jurnal.id_akun', 'left')->join('tbl_file', 'tbl_file.id_jurnal=tbl_jurnal.id_jurnal', 'left')->where('tbl_jurnal.publish', 'y')->group_by('tbl_file.id_jurnal')->get()->result();
+		$this->load->view('home/templates/header');
+		$this->load->view('home/arsip', $data);
+		$this->load->view('home/templates/footer');
+	}
+
+	public function download_jurnal($nama_file)
+	{
+		force_download('./assets/upload/jurnal/arsip/' . $nama_file, NULL);
 		redirect('home/arsip');
 	}
 }
